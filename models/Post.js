aae22f1c083ec7ea7dc40d36f5a7f8800a586608
ezziagheith <ezziagheith@gmail.com>
2019-11-20 15:08:35 -0800

    Post Schema

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const PostSchema = mongoose.Schema({
+    city: {
+        type: String,
+        required: [true, 'City is required'],
+    },
+    title: {
+        type: String,
+        required: [true, 'Title is required'],
+    },
+    author: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    body: {
+        type: String,
+        required: [true, 'Body is required'],
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+    // ANCHOR 
+    // comment: [],
+})
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
