d6586f3a145f326aabe70b75a942717c6542f1ad
ezziagheith <ezziagheith@gmail.com>
2019-11-21 11:59:21 -0800

    Added Show One route

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const ctrl = require('../controllers');
 
 
 router.get('/', ctrl.posts.showPost),
+router.get('/:postId', ctrl.posts.showOnePost),
 router.post('/', ctrl.posts.createPost),
 router.put('/', ctrl.posts.updatePost),
 router.delete('/', ctrl.posts.destroy)
