31740d2c45761c3f1f768990b067f45010f124d8
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-24 18:04:37 -0800

    update env string

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const dbUrl = process.env.MONGO_URI;
+mongoose.connect( process.env.MONGO_URI || "mongodb://localhost:27017/supreme-emperor-api" );
 
 
 mongoose.connect(dbUrl, {
