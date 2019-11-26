228f0ee03f37791292f8ce1fba4cd2bec27b666d
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-25 16:43:54 -0800

    updates cors to add heroku for front end

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const routes = require('./routes');
 // CORS - Cross Origin Resource Sharing
 
 const corsOptions = {
-    origin: [`http://localhost:3000`],
+    origin: [`http://localhost:3000`,'https://supreme-emperor-react.herokuapp.com/'],
     credentials: true,
     optionsSuccessStatus: 200
 };
